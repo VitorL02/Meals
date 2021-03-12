e6a676da44a8e247c7c3653089e3ba40beebf308
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-03-12 19:39:07 -0300

    Criação das telas de Categorias,estilização da mesma e colocando exibindo em uma tela,a tela inicial das refeições

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,27 @@
 import 'package:flutter/material.dart';
+import 'screens/categoriesScreen.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MealsApp());
 
 class MealsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Vamos Cozinhar?', home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text(
-            'Vamos Cozinhar?',
-            style:
-                TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.red[700]),
-      body: Center(
-        child: Text('Inicio'),
+    return MaterialApp(
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
+      home: CategoriesScreen(),
     );
   }
 }
--- lib/screens/categoriesScreen.dart
+++ lib/screens/categoriesScreen.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import '../components/categoryItem.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      //SliverGridDelegateWithMaxCrossAxisExtent = Area de scroll definida  pelo maximo cross
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Vamos Cozinhar',
+          style: TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.red[700],
       ),
-      children: <Widget>[],
+      body: GridView(
+          padding: const EdgeInsets.all(25),
+          //SliverGridDelegateWithMaxCrossAxisExtent = Area de scroll definida  pelo maximo cross
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES.map((cat) {
+            return CategoryItem(cat);
+          }).toList()),
     );
   }
 }
