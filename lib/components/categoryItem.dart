4b178c6faaf570ae3f864455af1a48c461723264
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-03-13 11:38:36 -0300

    Criando o inicio da navegação de telas,utilizando o Navigator

--- lib/components/categoryItem.dart
+++ lib/components/categoryItem.dart
@@ -1,3 +1,4 @@
+import 'package:Meals/screens/categoriesMelsScreen.dart';
 import 'package:flutter/material.dart';
 import '../models/category.dart';
 
@@ -5,20 +6,35 @@ class CategoryItem extends StatelessWidget {
   final Category category;
   CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    //Pelo navigator ,uma pilha de telas sera criada,uma em cima da outra,assim navegando entre telas
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoriesMelsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-            colors: [category.color.withOpacity(0.5), category.color],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
+    //InkWell torna o componente item clicavel
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+              colors: [category.color.withOpacity(0.5), category.color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
       ),
     );
   }
