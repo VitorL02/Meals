d1a0f4840c568d2c56bff6a150cae19a1da28e9d
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-03-22 14:55:27 -0300

    Mudança no scroll da pagina,acabando com problema de overflow

--- lib/screens/mealDetail.dart
+++ lib/screens/mealDetail.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
   Widget _createSectionTitle(BuildContext context, String title) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.title,
-        ),
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
       ),
     );
   }
@@ -40,50 +38,47 @@ class MealDetailScreen extends StatelessWidget {
           ),
           backgroundColor: Colors.red[700],
         ),
-        body: Column(children: [
-          Container(
-            height: 160,
-            width: double.infinity,
-            child: Image.network(meal.imageUrl, fit: BoxFit.cover),
-          ),
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: ListView(children: <Widget>[
-              _createSectionTitle(context, meal.title),
-              _createSectionContainer(ListView.builder(
-                  itemCount: meal.ingredients.length,
-                  itemBuilder: (ctx, index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 10),
-                        child: Text(meal.ingredients[index]),
-                      ),
-                      color: Colors.redAccent,
-                    );
-                  })),
-              _createSectionTitle(context, 'Passos'),
-              _createSectionContainer(
-                ListView.builder(
-                  itemCount: meal.steps.length,
-                  itemBuilder: (ctx, index) {
-                    return Column(
-                      children: [
-                        ListTile(
-                          leading: CircleAvatar(
-                            child: Text('${index + 1}'),
-                          ),
-                          title: Text(meal.steps[index]),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              height: 160,
+              width: double.infinity,
+              child: Image.network(meal.imageUrl, fit: BoxFit.cover),
+            ),
+            _createSectionTitle(context, meal.title),
+            _createSectionContainer(ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                    color: Colors.redAccent,
+                  );
+                })),
+            _createSectionTitle(context, 'Passos'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
                         ),
-                        Divider(),
-                      ],
-                    );
-                  },
-                ),
+                        title: Text(meal.steps[index]),
+                      ),
+                      //Cria uma linha que divide os componentes
+                      Divider(),
+                    ],
+                  );
+                },
               ),
-            ]),
-          )
-        ]));
+            )
+          ]),
+        ));
   }
 }
